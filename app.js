21a9a90a4c3910ca0bb98b5eb5ff8a05cdcd11a0
xiaoz <1002945291@qq.com>
2018-07-10 19:21:40 +0800

    进一步完善了nodejs静态资源服务器

--- app.js
+++ app.js
@@ -5,27 +5,36 @@ let http=require('http');
 let path=require('path');
 // 引入文件模块
 let fs=require('fs');
+// 引入请求头格式,第三方插件
+let mime=require('mime');
+// 引入查询字符串模块
+let querystring=require('querystring');
+
 // 创建文件根目录
 let rootPath=path.join(__dirname,'www');
 
 // 创建web服务
 http.createServer((request,response)=>{   
-    // 根据url请求，生成绝对路径
-    let filePath=path.join(rootPath,request.url);
+    // 根据url请求，生成绝对路径 querystring.unescape()把url里的中文编码解码
+    let filePath=path.join(rootPath,querystring.unescape(request.url));
     // 判断有没有文件
     let fsExists=fs.existsSync(filePath);
-    
+    console.log(filePath);
     if(fsExists){
         // 生成目录列表
         fs.readdir(filePath,(err,files)=>{
+            
             if(err){
                 // 进来这里，说明是文件
+                
                 fs.readFile(filePath,(err,data)=>{
+                    response.writeHead(200,{
+                        'content-type':mime.getType(filePath)
+                    })
                     response.end(data);
                 })
             }else{
                 // 进来这里，说明是文件夹
-
                 //有首页，直接去首页
                 if(files.indexOf('index.html')!=-1){
                     
@@ -35,11 +44,13 @@ http.createServer((request,response)=>{
                 }else{
                     // 遍历出文件
                     let datastr='';
+                    
                     for(let i=0;i<files.length;i++){
-                        datastr+=`<h2><a href='./${files[i]}'>${files[i]}</a></h2>`;
+                        datastr+=`<h2><a href='${request.url=='/'?'':request.url}/${files[i]}'>${files[i]}</a></h2>`;
                     }
                     response.writeHead(200,{
-                        'content-type':'text/html;charser=utf-8'
+                        // 判断是什么文件，设置什么格式的请求头
+                        'content-type':'text/html;charset=utf-8'
                     });
                     response.end(datastr);
                 }
